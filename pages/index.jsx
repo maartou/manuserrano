1b10437bfb8a5632c01397766c5113e2d77d3be6
Martin Padron <martinepadronv@gmail.com>
2022-09-07 17:03:19 -0400

    Agregando google analytics

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 
 import Header from "../components/header/Header";
 import Hero from "../components/hero/Hero";
@@ -11,14 +12,36 @@ import Footer from "../components/footer/Footer";
 export default function Home() {
   return (
     <>
+      <Script
+        async
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-GLYDPSK39B"
+      ></Script>
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+  
+    gtag('config', 'G-GLYDPSK39B');
+  `}
+      </Script>
+
       <Head>
         <title>Manuela Serrano | De camino al Berklee</title>
         <link rel="shortcut icon" href="favicon.svg" type="image/x-icon" />
-        <meta name="description" content="Hola soy Manu! Músico y Cantautora venezolana: Especialista en Música Moderna y Productora Musical en formación, escucha mi historia y acómpañame de camino al Berklee." />
-        <meta name="keywords" content="Manuela Serrano, Manuela Serrano Berklee, De camino al Berklee, Berklee college of music, Berklee, Manuela, Manu" />
+        <meta
+          name="description"
+          content="Hola soy Manu! Músico y Cantautora venezolana: Especialista en Música Moderna y Productora Musical en formación, escucha mi historia y acómpañame de camino al Berklee."
+        />
+        <meta
+          name="keywords"
+          content="Manuela Serrano, Manuela Serrano Berklee, De camino al Berklee, Berklee college of music, Berklee, Manuela, Manu"
+        />
       </Head>
 
-      <Header/>
+      <Header />
 
       <main>
         <Hero />
